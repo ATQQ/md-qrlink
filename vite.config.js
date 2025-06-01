e6fef46d95b0715deb7d548f448afd873aa120d6
sugar <engineerzjl@foxmail.com>
2025-06-01 20:36:14 +0800

    chore: 调整资源构建

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.GITHUB_ACTIONS === 'true' ? '/md-qrlink/' : '/',
+  // 便于 掘金 AI Coding 复用资源
+  // https://aicoding.juejin.cn/aicoding/work/7510906446952071202
+  build: isGithubActions ? {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  } : undefined,
+  base: isGithubActions ? '/md-qrlink/' : '/',
 })
