d3bba751974e3adbdf1c2e94a5919c71cd19a34d
sugar <engineerzjl@foxmail.com>
2025-06-06 17:48:17 +0800

    feat: 适配掘金 aicoding

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+const isJuejin = process.env.JUEJIN === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  // 便于 掘金 AI Coding 复用资源
-  // https://aicoding.juejin.cn/aicoding/work/7510906446952071202
-  build: isGithubActions ? {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  } : undefined,
+  // 便于 掘金 AI Coding 使用
+  plugins: isJuejin ? [vue(), viteSingleFile()] : [vue()],
   base: isGithubActions ? '/md-qrlink/' : '/',
 })
